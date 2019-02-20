05950bdf87e22d5bdc1e7aedc8b63365c7006255
nyirahabineza <habcecile@gmail.com>
2019-02-20 11:18:11 +0200

    adding the get_source_data=url.read()request.py

--- app/request.py
+++ app/request.py
@@ -14,7 +14,7 @@ def get_sources(category):
     get_sources_url = source_base_url.format(category,api_key)
     print(get_sources_url)
     with urllib.request.urlopen(get_sources_url) as url:
-        get_sources_data = url.read()
+        
         get_sources_response = json.loads(get_sources_data)
 
         source_results = None
